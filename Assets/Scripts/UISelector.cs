af9937e494db781f1fc9bcf5973e5db74928cd43
María Lourdes Garcia Florez <marylo281292@gmail.com>
2019-01-26 11:35:56 -0400

    script ui

---
+++ Assets/Scripts/UISelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelector : MonoBehaviour
+{
+    public GameObject actualObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void selectPrefab(GameObject obj) {
+        actualObject = obj;
+        Debug.Log(obj.name);
+    }
+}
