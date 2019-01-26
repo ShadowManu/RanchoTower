25de14e25f33a2edd7be53226c4cb58aeb49d284
Gamar <gamarazuaje@gmail.com>
2019-01-26 03:09:32 -0400

    Added ResourceManager

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour
+{
+    
+    public Resource gold;
+    public int goldPerSecond;
+
+    float timeInterval = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gold = GameObject.Find("Gold").GetComponent<Resource>();
+        gold.set(100);
+
+        InvokeRepeating("UpdateGold", 0f, 1f);
+
+    }
+
+    // Update is called once per frame
+    void UpdateGold()
+    {
+            getGoldPerSecond();
+            updateGold();
+    }
+
+    void getGoldPerSecond(){
+        goldPerSecond = 1;
+    }
+
+    void updateGold(){
+        gold.amount = gold.amount + goldPerSecond;
+    }
+}
---
+++ Assets/Scripts/ResourceViewer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceViewer : MonoBehaviour{ 
+
+    public Text amountText;
+    public Resource gold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amountText = GameObject.Find("ResourceText").GetComponent<Text>();
+        gold = GameObject.Find("Gold").GetComponent<Resource>();
+        Debug.Log(gold.amount);
+        SetAmountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetAmountText();
+    }
+
+    void SetAmountText(){
+        amountText.text = "Gold: " + gold.amount.ToString ();
+    }
+}
