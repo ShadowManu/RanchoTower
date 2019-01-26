0cd6d6d4bee64ddead57f8ce8a7e490c3d501b8e
JoseD92 <jose_daniel_d@hotmail.com>
2019-01-26 13:50:25 -0400

    spawner script

---
+++ Assets/scripts/spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject Mob;
+
+    public float InitialCoolDown;
+
+    public float WaveCooldown;
+
+    public int WaveAmount;
+
+    private float NextWaveTime;
+
+    private int ToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.NextWaveTime = this.InitialCoolDown;
+        this.ToSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.NextWaveTime -= Time.deltaTime;
+        if (this.NextWaveTime <= 0) this.ToSpawn += this.WaveAmount;
+    }
+
+    void OnCollision()
+    {
+
+    }
+}
