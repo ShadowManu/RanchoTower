162cf192db22f58b9eb0f4dc721eef757041ede5
JoseD92 <jose_daniel_d@hotmail.com>
2019-01-26 15:32:10 -0400

    more to spawner

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -16,22 +16,42 @@ public class spawner : MonoBehaviour
 
     private int ToSpawn;
 
+    private int EnemyOnSpawn;
+
+    public GameObject EnemyTarget;
+
     // Start is called before the first frame update
     void Start()
     {
         this.NextWaveTime = this.InitialCoolDown;
         this.ToSpawn = 0;
+        this.EnemyOnSpawn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.NextWaveTime -= Time.deltaTime;
-        if (this.NextWaveTime <= 0) this.ToSpawn += this.WaveAmount;
+        if (this.NextWaveTime <= 0)
+        {
+            this.ToSpawn += this.WaveAmount;
+            this.NextWaveTime += this.WaveCooldown;
+        }
+        if (this.ToSpawn > 0 && this.EnemyOnSpawn == 0)
+        {
+            var newObj = Instantiate(Mob);
+            ((EnemyBehavior)newObj.GetComponentInChildren(typeof(EnemyBehavior))).target = EnemyTarget;
+            newObj.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+            this.ToSpawn--;
+        }
     }
 
-    void OnCollision()
+    void OnCollisionEnter(Collision col)
     {
-
+        this.EnemyOnSpawn += col.gameObject.tag == "Enemy" ? 1 : 0;
+    }
+    void OnCollisionExit(Collision col)
+    {
+        this.EnemyOnSpawn -= col.gameObject.tag == "Enemy" ? 1 : 0;
     }
 }
