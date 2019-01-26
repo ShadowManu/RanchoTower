81f932d31bd525eb67519855e0a6d85e50fa69a8
Zuil <zuilnorris@gmail.com>
2019-01-26 14:47:47 -0400

    AudioMixer, Audio Manager and Slider controlls complete

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void StartGame()
+	{
+		//Loads next scene in queue
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quiting...");
+		Application.Quit();
+	}
+
+}
