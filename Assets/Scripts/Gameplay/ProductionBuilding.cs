0fca0792b0962d30d375e74145036158ee9243d0
Manuel Pacheco <manuelalejandropm@gmail.com>
2019-01-27 06:52:02 -0400

    move and adjust files

---
+++ Assets/Scripts/Gameplay/ProductionBuilding.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductionBuilding : MonoBehaviour
+{
+  public int productionRate = 1;
+}
