b4fe46748c14eb96e4951048132f31a5b096a263
JoseD92 <jose_daniel_d@hotmail.com>
2019-01-27 14:58:13 -0400

    walls and shadows

--- Assets/Scripts/Grid/Wall.cs
+++ Assets/Scripts/Grid/Wall.cs
@@ -4,48 +4,147 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour, IGridObject
 {
-  private readonly bool[,] shape = new bool[,] { { true } };
-  public int gridPosX;
-  public int gridPosY;
+    private readonly bool[,] shape = new bool[,] { { true } };
+    public int gridPosX;
+    public int gridPosY;
 
-  public float price;
-  public float Price() { return price; }
+    public float price;
+    public float Price() { return price; }
 
-  public void SetGridPosition(int x, int y)
-  {
-    this.gridPosX = x;
-    this.gridPosY = y;
-  }
+    public void SetGridPosition(int x, int y)
+    {
+        this.gridPosX = x;
+        this.gridPosY = y;
+    }
 
-  public Tuple<bool[,], Tuple<int, int>> Space()
-  {
-    return new Tuple<bool[,], Tuple<int, int>>(shape, new Tuple<int, int>(shape.GetLength(0), shape.GetLength(1)));
-  }
+    public Tuple<bool[,], Tuple<int, int>> Space()
+    {
+        return new Tuple<bool[,], Tuple<int, int>>(shape, new Tuple<int, int>(shape.GetLength(0), shape.GetLength(1)));
+    }
 
-  public Tuple<int, int> GridPosition()
-  {
-    return new Tuple<int, int>(gridPosX, gridPosY);
-  }
+    public Tuple<int, int> GridPosition()
+    {
+        return new Tuple<int, int>(gridPosX, gridPosY);
+    }
 
-  public void kill()
-  {
-    Destroy(transform.parent.gameObject);
-  }
+    public void kill()
+    {
+        Destroy(transform.parent.gameObject);
+    }
 
-  public string Tag()
-  {
-    return this.gameObject.tag;
-  }
+    public string Tag()
+    {
+        return this.gameObject.tag;
+    }
 
-  // Start is called before the first frame update
-  void Start()
-  {
+    // Start is called before the first frame update
+    void Start()
+    {
 
-  }
+    }
 
-  // Update is called once per frame
-  void Update()
-  {
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 6; i++) transform.parent.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        bool top = Grid.instance.CellTag(gridPosX, gridPosY + 1) == "Building";
+        bool down = Grid.instance.CellTag(gridPosX, gridPosY - 1) == "Building";
+        bool left = Grid.instance.CellTag(gridPosX - 1, gridPosY) == "Building";
+        bool right = Grid.instance.CellTag(gridPosX + 1, gridPosY) == "Building";
 
-  }
+        if (top && down && left && right)
+        {
+            transform.parent.gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 0;
+            return;
+        }
+
+        if (top && down && left)
+        {
+            transform.parent.gameObject.transform.GetChild(4).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 0;
+            return;
+        }
+        if (top && left && right)
+        {
+            transform.parent.gameObject.transform.GetChild(4).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * -90;
+            return;
+        }
+        if (top && down && right)
+        {
+            transform.parent.gameObject.transform.GetChild(4).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 180;
+            return;
+        }
+        if (down && left && right)
+        {
+            transform.parent.gameObject.transform.GetChild(4).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 90;
+            return;
+        }
+
+        if (down && top)
+        {
+            transform.parent.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 0;
+            return;
+        }
+        if (left && right)
+        {
+            transform.parent.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 90;
+            return;
+        }
+
+        if (top && right)
+        {
+            transform.parent.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * -90;
+            return;
+        }
+        if (top && left)
+        {
+            transform.parent.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 0;
+            return;
+        }
+        if (down && right)
+        {
+            transform.parent.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 180;
+            return;
+        }
+        if (down && left)
+        {
+            transform.parent.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 90;
+            return;
+        }
+
+        if (top)
+        {
+            transform.parent.gameObject.transform.GetChild(5).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 270;
+            return;
+        }
+        if (down)
+        {
+            transform.parent.gameObject.transform.GetChild(5).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 90;
+            return;
+        }
+        if (right)
+        {
+            transform.parent.gameObject.transform.GetChild(5).gameObject.SetActive(true);
+            transform.parent.eulerAngles = Vector3.up * 180;
+            return;
+        }
+        if (left)
+        {
+            transform.parent.gameObject.transform.GetChild(5).gameObject.SetActive(true);
+            return;
+        }
+        transform.parent.gameObject.transform.GetChild(3).gameObject.SetActive(true);
+    }
 }
--- Assets/Scripts/Placer.cs
+++ Assets/Scripts/Placer.cs
@@ -4,68 +4,78 @@ using UnityEngine;
 
 public class Placer : MonoBehaviour
 {
-  private GameObject Prefab;
-  public Grid Grid;
+    private GameObject Prefab;
 
-  public Material CanPlace;
+    public Grid Grid;
 
-  public Material NoPlace;
+    public Material CanPlace;
 
+    public Material NoPlace;
 
+    public GameObject[] Prefabs;
 
-  private GameObject intancedO;
+    public GameObject[] PrefabsShadows;
 
-  private Material instancedOriginalM;
+    private GameObject intancedO;
 
-  // Start is called before the first frame update
-  void Start()
-  {
+    private Material instancedOriginalM;
 
-  }
-
-  public void SetPrefab(GameObject _Prefab)
-  {
-    this.Prefab = _Prefab;
-    intancedO = Instantiate(Prefab);
-    instancedOriginalM = intancedO.transform.GetChild(0)
-        .GetComponent<MeshRenderer>().material;
-  }
-
-  public void unsetPrefab()
-  {
-    Destroy(intancedO);
-    this.Prefab = null;
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetMouseButtonDown(1))
+    // Start is called before the first frame update
+    void Start()
     {
-      this.unsetPrefab();
+
     }
-    if (Prefab != null)
+
+    public void SetPrefab(int index)
     {
-      var pos = Grid.CurrentCellPosition();
-      var pos2 = Grid.CellPosition(pos.Item1.Item1, pos.Item1.Item2);
-      intancedO.transform.position = new Vector3(pos2.x, 0, pos2.y);
-      var igrid = (IGridObject)intancedO.GetComponentInChildren(typeof(IGridObject));
-      if (Grid.CanAdd(igrid, pos.Item1.Item1, pos.Item1.Item2))
-      {
-        intancedO.transform.GetChild(0).GetComponent<MeshRenderer>().material = CanPlace;
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        this.Prefab = Prefabs[index];
+        intancedO = Instantiate(PrefabsShadows[index]);
+        instancedOriginalM = intancedO.transform.GetChild(0)
+            .GetComponent<MeshRenderer>().material;
+    }
+
+    public void unsetPrefab()
+    {
+        Destroy(intancedO);
+        this.Prefab = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
         {
-          Grid.Put(igrid, pos.Item1.Item1, pos.Item1.Item2);
-          igrid.SetGridPosition(pos.Item1.Item1, pos.Item1.Item2);
-          intancedO.transform.GetChild(0).GetComponent<MeshRenderer>().material = instancedOriginalM;
-          Prefab = null;
-          intancedO = null;
+            this.unsetPrefab();
         }
-      }
-      else
-      {
-        intancedO.transform.GetChild(0).GetComponent<MeshRenderer>().material = NoPlace;
-      }
+        if (Prefab != null)
+        {
+            var pos = Grid.CurrentCellPosition();
+            var pos2 = Grid.CellPosition(pos.Item1.Item1, pos.Item1.Item2);
+            intancedO.transform.position = new Vector3(pos2.x, 0, pos2.y);
+            var igrid = (IGridObject)intancedO.GetComponentInChildren(typeof(IGridObject));
+            igrid.SetGridPosition(pos.Item1.Item1, pos.Item1.Item2);
+            if (Grid.CanAdd(igrid, pos.Item1.Item1, pos.Item1.Item2))
+            {
+                var meshrenderers = intancedO.GetComponentsInChildren<MeshRenderer>();
+                foreach (MeshRenderer mr in meshrenderers) mr.material = CanPlace;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    var temp = Instantiate(this.Prefab);
+                    temp.transform.position = new Vector3(pos2.x, 0, pos2.y);
+                    var tempIgrid = (IGridObject)temp.GetComponentInChildren(typeof(IGridObject));
+                    tempIgrid.SetGridPosition(pos.Item1.Item1, pos.Item1.Item2);
+                    Grid.Put(tempIgrid, pos.Item1.Item1, pos.Item1.Item2);
+                    igrid.SetGridPosition(pos.Item1.Item1, pos.Item1.Item2);
+                    intancedO.transform.GetChild(0).GetComponent<MeshRenderer>().material = instancedOriginalM;
+                    Prefab = null;
+                    Destroy(intancedO);
+                }
+            }
+            else
+            {
+                var meshrenderers = intancedO.GetComponentsInChildren<MeshRenderer>();
+                foreach (MeshRenderer mr in meshrenderers) mr.material = NoPlace;
+            }
+        }
     }
-  }
 }
